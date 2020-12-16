cc1cc986bcc3c8992a713b7b0bc3bf3e36a71f77
FSpruhs <spruhs.com>
2020-12-16 17:58:09 +0100

    day 8

--- 8/8 (Kopie).py
+++ 8/8 (Kopie).py
@@ -1,56 +1,71 @@
-daten = []
-figur = 0
-acc_zahl=0
+import math 
+import functools
+import re
+import random
+import collections
 
-datei = open("Daten.txt","r")
-r=datei.read().splitlines()
-datei.close()
-for satz in r:
-    daten.append(satz)
+class Instruction:
+    def __init__(self, op, arg):
+        self.op = op
+        self.arg = arg
+
+def solve1(code):
+    line = 0
+    acc = 0
+    seen = set()
+
+    while True:
+        if line in seen or line >= len(code):
+            print(acc)
+            break
+        seen.add(line)    
+        inst = code[line]
+
+        if inst.op == "acc":
+            acc += inst.arg
+            line += 1
+        elif inst.op == "jmp":
+            line += inst.arg
+        elif inst.op == "nop":
+            line += 1
+
+def solve2(code):
+    # Try every line, and swap nops for jmp
+    for i in range(len(code)):
+        if code[i].op == "nop" or code[i].op == "jmp":
+            # Swap the op
+            code[i].op = "nop" if code[i].op == "jmp" else "jmp"
+
+            # Run the program!
+            line = 0
+            acc = 0
+            seen = set()
+
+            while True:
+                if line in seen:
+                    break
+                if line == len(code):
+                    print(acc)
+                    return
+
+                seen.add(line)    
+                inst = code[line]
+
+                if inst.op == "acc":
+                    acc += inst.arg
+                    line += 1
+                elif inst.op == "jmp":
+                    line += inst.arg
+                elif inst.op == "nop":
+                    line += 1
+
+            # Switch back
+            code[i].op = "nop" if code[i].op == "jmp" else "jmp"
 
 
-def nop():
-    global figur
-    figur += 1
-
-def acc(vorzeichen, zahl):
-    global acc_zahl, figur
-    if vorzeichen == "+":
-        acc_zahl += int(zahl)
-    else:
-        acc_zahl -= int(zahl)
-    figur += 1
-
-def jmp(vorzeichen, zahl):
-    global figur
-    if vorzeichen == "+":
-        figur += int(zahl)
-    else:
-        figur -= int(zahl)
-
-def var_erstellen(l):
-    befehl = l[0:3]
-    vorzeichen = l[4:5]
-    zahl = l[5:]
-    return befehl, vorzeichen, zahl
-
-while figur != "END":
-    print(daten[figur])
-    befehl, vorzeichen, zahl = var_erstellen(daten[figur])
-    daten[figur] = "END"
-    if befehl == "nop":
-        nop()
-    elif befehl == "acc":
-        acc(vorzeichen, zahl)
-    elif befehl == "jmp":
-        jmp(vorzeichen, zahl)
-    else:
-        print("Fehler")
-        break
-print(daten)
-print(acc_zahl)
-
-    
-    
-        
-    
+if __name__ == "__main__":
+    with open('Daten.txt') as f:
+        data = f.read().splitlines()
+        code = list(map(lambda x: Instruction(x.split()[0], int(x.split()[1])), data))
+        solve1(code)
+        solve2(code)
