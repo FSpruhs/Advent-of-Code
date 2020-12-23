4869e18b5605a1b40690b941ceace2687fd0d8fb
FSpruhs <spruhs.com>
2020-12-23 17:33:31 +0100

    day 15

---
+++ 15/15.py
@@ -0,0 +1,24 @@
+#DATEN = [0,3,6]
+DATEN = [15,12,0,14,3,1]
+
+
+
+for i in range(6,2020):
+    aktuell = 0
+    last = DATEN[i-1]
+    if DATEN.count(last) > 1:
+        x=0
+        y=0
+        for j, k in reversed(list(enumerate(DATEN))):
+            if k == last and x == 0:
+                x=j
+            if k == last and x != j:
+                y=j
+                break
+        aktuell = x-y
+    DATEN.append(aktuell)
+    print(aktuell)
+
+    
+    
+    
